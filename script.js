b13a61a3a6012592bcc1a0d12342edb69dc54450
xuechi1 <89440260+xuechi1@users.noreply.github.com>
2025-02-03 16:09:44 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const menu = document.getElementById("menu");
+const closeButton = document.getElementById("close-mobile");
+const nav = document.getElementById("nav-mobile");
+const navLink = document.querySelectorAll(".nav-link");
+
+menu.addEventListener("click", () => {
+  nav.classList.add("show");
+});
+
+closeButton.addEventListener("click", () => {
+  nav.classList.remove("show");
+});
+
+navLink.forEach((link) => {
+  link.addEventListener("click", () => {
+    nav.classList.remove("show");
+  });
+});
